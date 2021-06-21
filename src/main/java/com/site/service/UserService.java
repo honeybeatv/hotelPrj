73424cb2cb36f67b968a855a3852ae8bef9a0165
ddoddu <777clf@naver.com>
2021-06-21 18:49:00 +0900

    윤수현> 회원 기본정보 호출 및 수정 완료 ㄴ UserInfoView, UserInfoModify   .jsp 완료

--- src/main/java/com/site/service/UserService.java
+++ src/main/java/com/site/service/UserService.java
@@ -6,4 +6,12 @@ import com.site.vo.UserVo;
 
 public interface UserService {
 
+	// 회원 기본정보 페이지 호출
+	UserVo UserInfoView(String userid);
+	// 회원 기본정보 수정페이지 호출
+	UserVo UserInfoModify(String userid);
+	// 회원 기본정보 수정페이지 실행
+	void UserInfoModifyDo(UserVo userVo);
+
+
 }
--- src/main/java/com/site/service/UserServiceImpl.java
+++ src/main/java/com/site/service/UserServiceImpl.java
@@ -10,5 +10,26 @@ import com.site.vo.UserVo;
 
 @Service
 public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	UserMapper userMapper;
+
+	@Override // 회원 기본정보 페이지 호출
+	public UserVo UserInfoView(String userid) {
+		UserVo userVo = userMapper.selectUserInfo(userid);
+		return userVo;
+	}
+
+	@Override // 회원 기본정보 수정페이지 호출
+	public UserVo UserInfoModify(String userid) {
+		UserVo userVo = userMapper.selectUserInfo(userid);
+		return userVo;
+	}
+
+	@Override // 회원 기본정보 수정페이지 실행
+	public void UserInfoModifyDo(UserVo userVo) {
+		userMapper.updateUserInfoModifyDo(userVo);
+	}
+
 
 }
