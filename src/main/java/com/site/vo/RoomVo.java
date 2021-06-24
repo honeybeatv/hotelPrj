47eb902ed3dde467e92b0f7ed21c0de548992fc1
ddoddu <777clf@naver.com>
2021-06-24 09:30:08 +0900

    윤수현> RoomVo 수정 user Category_hosting 작업중...

--- src/main/java/com/site/vo/RoomVo.java
+++ src/main/java/com/site/vo/RoomVo.java
@@ -8,8 +8,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RoomVo {
-	
-
 	private int roomNo;
 	private String rname;
 	private String rtype;
@@ -23,6 +21,5 @@ public class RoomVo {
 	private String rsmoke;
 	private String rpet;
 	private String raddress;
-
-	
+	private int userno;
 }
