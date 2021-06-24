bcf03410586ec9deb642450f0df07705884767cc
sungcheollim <sungcheollim@localhost>
2021-06-24 09:36:04 +0900

    r

--- src/main/java/com/site/vo/ReserveVo.java
+++ src/main/java/com/site/vo/ReserveVo.java
@@ -2,9 +2,16 @@ package com.site.vo;
 
 import java.sql.Timestamp;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import oracle.sql.TIMESTAMP;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReserveVo {
+	
 	private int re_no;
 	private int roomno;
 	private String status;
