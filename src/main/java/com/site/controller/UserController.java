603694134b77a56d59fc667467514daad7b51272
sungcheollim <sungcheollim@localhost>
2021-07-02 10:24:58 +0900

    숙소 추가 가능

--- src/main/java/com/site/controller/UserController.java
+++ src/main/java/com/site/controller/UserController.java
@@ -16,9 +16,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.site.service.UserService;
-import com.site.vo.UserReservationVo;
 import com.site.vo.RoomVo;
+import com.site.vo.UserReservationVo;
 import com.site.vo.UserVo;
 
 @Controller
